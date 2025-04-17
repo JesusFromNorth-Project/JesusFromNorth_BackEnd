497e575a8a900226b68586a9290563ee16b7dc59
RobMaster28 <180604094+RobMaster28@users.noreply.github.com>
2025-04-16 19:59:49 -0500

    inicializcion del proyecto clinica

---
+++ clinic_system/src/test/java/org/example/clinic_system/ClinicSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.clinic_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
