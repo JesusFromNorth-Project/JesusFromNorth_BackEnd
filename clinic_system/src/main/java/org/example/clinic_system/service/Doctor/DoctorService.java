d213920b11ee04249a4af4c799eebdfd16d43c99
Sergio Antonio <111243440+sergioantonioar@users.noreply.github.com>
2025-05-01 15:44:08 -0500

    agregando servicio doctor y comentando test

--- clinic_system/src/main/java/org/example/clinic_system/service/Doctor/DoctorService.java
+++ clinic_system/src/main/java/org/example/clinic_system/service/Doctor/DoctorService.java
@@ -1,4 +1,11 @@
 package org.example.clinic_system.service.Doctor;
 
+import org.example.clinic_system.model.Doctor;
+
 public interface DoctorService {
+    void save(Doctor doctor);
+    Doctor findByCmp(String cmp);
+    void delete(Doctor doctor);
+    Iterable<Doctor> findAll();
+
 }
