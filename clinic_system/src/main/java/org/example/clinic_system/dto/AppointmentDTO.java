4728cee8a4188ab76720ced3fbc40210cb5ea03f
Arnold J. <207950271+Arnisito@users.noreply.github.com>
2025-04-30 19:26:36 -0500

    agregado DTO de citas(AppointmentDTO)

---
+++ clinic_system/src/main/java/org/example/clinic_system/dto/AppointmentDTO.java
@@ -0,0 +1,27 @@
+package org.example.clinic_system.dto;
+
+import java.time.LocalDateTime;
+
+import org.example.clinic_system.model.Admin;
+import org.example.clinic_system.model.Doctor;
+import org.example.clinic_system.model.Patient;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class AppointmentDTO {
+
+    private LocalDateTime date_appointment;
+    private LocalDateTime date_attention;
+    private String description;
+    private Admin admin;
+    private Doctor doctor;
+    private Patient patient;
+
+}
