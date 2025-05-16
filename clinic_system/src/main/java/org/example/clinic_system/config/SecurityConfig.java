ab64b06167a045002ace989258898e3b2a006c74
AguidoRin <150485648+Aguido-rin@users.noreply.github.com>
2025-05-16 18:14:13 -0500

    all OK

--- clinic_system/src/main/java/org/example/clinic_system/config/SecurityConfig.java
+++ clinic_system/src/main/java/org/example/clinic_system/config/SecurityConfig.java
@@ -65,7 +65,11 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://127.0.0.1:5500"));
+        configuration.setAllowedOrigins(List.of(
+                "http://127.0.0.1:5500",
+                "http://localhost:5500",
+                "http://192.168.18.55:5500"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
