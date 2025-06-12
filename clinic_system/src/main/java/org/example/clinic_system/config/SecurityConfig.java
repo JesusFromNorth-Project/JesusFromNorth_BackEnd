a13c0648ec92a7422e1776306f5934577acc6993
AguidoRin <150485648+Aguido-rin@users.noreply.github.com>
2025-06-11 22:47:30 -0500

    configFinished

--- clinic_system/src/main/java/org/example/clinic_system/config/SecurityConfig.java
+++ clinic_system/src/main/java/org/example/clinic_system/config/SecurityConfig.java
@@ -41,12 +41,11 @@ public class SecurityConfig {
                                 "/auth/login",
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
-                                "/swagger-ui.html"
-                        ).permitAll()
+                                "/swagger-ui.html")
+                        .permitAll()
                         .requestMatchers("/doctor/**", "/service/**", "/specialty/**").hasRole("ADMIN")
                         .requestMatchers("/attention/**").hasRole("DOCTOR")
-                        .anyRequest().authenticated()
-                )
+                        .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
@@ -64,11 +63,11 @@ public class SecurityConfig {
         return daoAuthenticationProvider;
     }
 
-
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://127.0.0.1:5500"));
+        configuration
+                .setAllowedOrigins(List.of("http://127.0.0.1:5500", "http://localhost:5500", "http://localhost:5501"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
@@ -78,4 +77,3 @@ public class SecurityConfig {
         return source;
     }
 }
-
