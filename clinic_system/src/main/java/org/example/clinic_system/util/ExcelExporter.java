93e0122ffe74781573badf7de5abdf879216873f
Sergio Antonio <111243440+sergioantonioar@users.noreply.github.com>
2025-06-11 20:03:29 -0500

    modificando generador excel

--- clinic_system/src/main/java/org/example/clinic_system/util/ExcelExporter.java
+++ clinic_system/src/main/java/org/example/clinic_system/util/ExcelExporter.java
@@ -1,6 +1,7 @@
 package org.example.clinic_system.util;
 
 import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.util.IOUtils;
 import org.apache.poi.xssf.usermodel.DefaultIndexedColorMap;
 import org.apache.poi.xssf.usermodel.XSSFCellStyle;
 import org.apache.poi.xssf.usermodel.XSSFColor;
@@ -8,10 +9,9 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.example.clinic_system.dto.entityDTO.DoctorDTO;
 import org.example.clinic_system.dto.entityDTO.PatientDTO;
 import org.example.clinic_system.model.Patient;
+import org.springframework.core.io.ClassPathResource;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.List;
 
 public class ExcelExporter {
@@ -35,6 +35,22 @@ public class ExcelExporter {
         try (Workbook workbook = new XSSFWorkbook(); ByteArrayOutputStream out = new ByteArrayOutputStream()) {
             Sheet sheet = workbook.createSheet("Pacientes");
 
+            // Cargar imagen desde resources
+            ClassPathResource imgFile = new ClassPathResource("LogoCJDN.png");
+            byte[] imageBytes = IOUtils.toByteArray(imgFile.getInputStream());
+            int pictureIdx = workbook.addPicture(imageBytes, Workbook.PICTURE_TYPE_PNG);
+
+            CreationHelper helper = workbook.getCreationHelper();
+            Drawing<?> drawing = sheet.createDrawingPatriarch();
+            ClientAnchor anchor = helper.createClientAnchor();
+
+            // Centrar imagen (entre columnas 3 y 9, en fila 0)
+            anchor.setCol1(4); // Columna D
+            anchor.setRow1(0); // Fila 0
+
+            Picture pict = drawing.createPicture(anchor, pictureIdx);
+            pict.resize(3.0); // Ajusta el tamaño según tus necesidades
+
             // Crear estilo para la cabecera
             CellStyle headerStyle = workbook.createCellStyle();
             headerStyle.setFillForegroundColor(IndexedColors.DARK_GREEN.getIndex());
@@ -51,7 +67,7 @@ public class ExcelExporter {
             headerStyle.setVerticalAlignment(VerticalAlignment.CENTER);
 
             // Crear fila de cabecera
-            Row headerRow = sheet.createRow(0);
+            Row headerRow = sheet.createRow(3);
             for (int col = 0; col < columns.length; col++) {
                 Cell cell = headerRow.createCell(col);
                 cell.setCellValue(columns[col]);
@@ -59,11 +75,11 @@ public class ExcelExporter {
             }
 
             // Datos
-            int rowIdx = 1;
+            int rowIdx = 4;
             for (PatientDTO patient : patients) {
                 Row row = sheet.createRow(rowIdx);
 
-                row.createCell(0).setCellValue(rowIdx);
+                row.createCell(0).setCellValue(rowIdx - 3);
                 row.createCell(1).setCellValue(patient.getFirst_name());
                 row.createCell(2).setCellValue(patient.getLast_name());
                 row.createCell(3).setCellValue(patient.getEmail());
@@ -94,6 +110,8 @@ public class ExcelExporter {
                 "ID",
                 "Nombre",
                 "Apellido",
+                "CMP",
+                "Especialidad",
                 "Email",
                 "Dirección",
                 "Teléfono",
@@ -103,6 +121,22 @@ public class ExcelExporter {
 
         try (Workbook workbook = new XSSFWorkbook(); ByteArrayOutputStream out = new ByteArrayOutputStream()) {
             Sheet sheet = workbook.createSheet("Doctores");
+
+            // Cargar imagen desde resources
+            ClassPathResource imgFile = new ClassPathResource("LogoCJDN.png");
+            byte[] imageBytes = IOUtils.toByteArray(imgFile.getInputStream());
+            int pictureIdx = workbook.addPicture(imageBytes, Workbook.PICTURE_TYPE_PNG);
+
+            CreationHelper helper = workbook.getCreationHelper();
+            Drawing<?> drawing = sheet.createDrawingPatriarch();
+            ClientAnchor anchor = helper.createClientAnchor();
+
+            // Centrar imagen (entre columnas 3 y 9, en fila 0)
+            anchor.setCol1(4); // Columna D
+            anchor.setRow1(0); // Fila 0
+
+            Picture pict = drawing.createPicture(anchor, pictureIdx);
+            pict.resize(3.0); // Ajusta el tamaño según tus necesidades
 
             // Crear estilo para la cabecera
             CellStyle headerStyle = workbook.createCellStyle();
@@ -120,7 +154,7 @@ public class ExcelExporter {
             headerStyle.setVerticalAlignment(VerticalAlignment.CENTER);
 
             // Crear fila de cabecera
-            Row headerRow = sheet.createRow(0);
+            Row headerRow = sheet.createRow(3);
             for (int col = 0; col < columns.length; col++) {
                 Cell cell = headerRow.createCell(col);
                 cell.setCellValue(columns[col]);
@@ -128,18 +162,20 @@ public class ExcelExporter {
             }
 
             //Datos
-            int rowIdx = 1;
+            int rowIdx = 4;
             for (DoctorDTO doctor : doctors) {
                 Row row = sheet.createRow(rowIdx);
 
-                row.createCell(0).setCellValue(rowIdx);
+                row.createCell(0).setCellValue(rowIdx - 3);
                 row.createCell(1).setCellValue(doctor.getFirst_name());
                 row.createCell(2).setCellValue(doctor.getLast_name());
-                row.createCell(3).setCellValue(doctor.getEmail());
-                row.createCell(4).setCellValue(doctor.getAddress());
-                row.createCell(5).setCellValue(doctor.getPhone());
-                row.createCell(6).setCellValue(doctor.getLandline_phone());
-                row.createCell(7).setCellValue(doctor.getDni());
+                row.createCell(3).setCellValue(doctor.getCmp());
+                row.createCell(4).setCellValue(doctor.getSpecialty().getSpecialty_name());
+                row.createCell(5).setCellValue(doctor.getEmail());
+                row.createCell(6).setCellValue(doctor.getAddress());
+                row.createCell(7).setCellValue(doctor.getPhone());
+                row.createCell(8).setCellValue(doctor.getLandline_phone());
+                row.createCell(9).setCellValue(doctor.getDni());
 
                 rowIdx++;
             }
