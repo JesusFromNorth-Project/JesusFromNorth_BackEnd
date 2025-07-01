e9d3b202c20ca25e6d9377bf4f71c70682b365fc
AguidoRin <150485648+Aguido-rin@users.noreply.github.com>
2025-07-01 16:03:50 -0500

    addedPersonalIp

--- clinic_system/src/main/java/org/example/clinic_system/config/SecurityConfig.java
+++ clinic_system/src/main/java/org/example/clinic_system/config/SecurityConfig.java
@@ -68,7 +68,8 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration
-                .setAllowedOrigins(List.of("http://127.0.0.1:5500", "http://localhost:5500", "http://localhost:5501"));
+                .setAllowedOrigins(List.of("http://127.0.0.1:5500", "http://localhost:5500", "http://localhost:5501",
+                        "http://192.168.18.55:5501"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
