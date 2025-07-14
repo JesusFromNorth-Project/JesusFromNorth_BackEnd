5c55376012e5d335ff8acf3571545b587241484f
AguidoRin <150485648+Aguido-rin@users.noreply.github.com>
2025-07-14 18:50:27 -0500

    Formated

--- clinic_system/src/main/java/org/example/clinic_system/ClinicSystemApplication.java
+++ clinic_system/src/main/java/org/example/clinic_system/ClinicSystemApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ClinicSystemApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ClinicSystemApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(ClinicSystemApplication.class, args);
+  }
 
 }
